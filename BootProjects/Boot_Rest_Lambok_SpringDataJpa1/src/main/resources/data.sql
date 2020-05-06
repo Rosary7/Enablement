7ce776266182c8838f098576762e57393f68b237
Rosary7 <amirtrichy@gmail.com>
2020-05-06 19:26:01 +0530

    Add files via upload

---
+++ BootProjects/Boot_Rest_Lambok_SpringDataJpa1/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO TEST_EMPLOYEE (first_name, last_name, email_address) VALUES ('Peter', 'North', 'abc@gmail.com');
+INSERT INTO TEST_EMPLOYEE (first_name, last_name, email_address) VALUES ('Mani', 'Kumar', 'efg@gmail.com');
+
+INSERT INTO TEST_USER (first_name, last_name, email_address) VALUES ('Peter', 'North', 'abc@gmail.com');
+INSERT INTO TEST_USER (first_name, last_name, email_address) VALUES ('Mani', 'Kumar', 'efg@gmail.com'); 
