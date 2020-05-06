b470bd5f61c1bb92f6a0c590158a85aaa09632c8
Rosary7 <amirtrichy@gmail.com>
2020-05-06 19:18:25 +0530

    Add files via upload

---
+++ BootProjects/Boot_Rest_Lambok_SpringDataJpa1/src/main/java/com/sb/WebConfiguration.java
@@ -0,0 +1,17 @@
+package com.sb;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfiguration {
+	@Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/console/*");  // http://localhost:8080/console
+        return registrationBean;
+    }
+
+}
