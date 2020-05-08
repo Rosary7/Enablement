747b4d9c74e291a1e70fcb4e61af2f1111434c5e
Rosary7 <amirtrichy@gmail.com>
2020-05-08 20:32:04 +0530

    Add files via upload

---
+++ BootProjects/Boot_JpaRepository_Mappings/src/main/java/com/mappings/many2one/CommentRepository.java
@@ -0,0 +1,15 @@
+package com.mappings.many2one;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Page<Comment> findByPostId(Long postId, Pageable pageable);
+	//Optional<Comment> findByPostId(Long postId);
+    Optional<Comment> findByIdAndPostId(Long id, Long postId);
+}
---
+++ BootProjects/Boot_JpaRepository_Mappings/src/main/java/com/mappings/many2one/PostController.java
@@ -0,0 +1,54 @@
+package com.mappings.many2one;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @PostMapping("/createPost")
+    public Post createPost(@Valid @RequestBody Post post) {
+        return postRepository.save(post);
+    }
+
+
+    @GetMapping("/getAllPosts")
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+    
+    @PutMapping("/updatePost/{postId}")
+    public Post updatePost(@PathVariable Long postId, @Valid @RequestBody Post postRequest) {
+        return postRepository.findById(postId).map(post -> {
+            post.setTitle(postRequest.getTitle());
+            post.setDescription(postRequest.getDescription());
+            post.setContent(postRequest.getContent());
+            return postRepository.save(post);
+        }).orElseThrow(() -> new ResourceNotFoundException("PostId " + postId + " not found"));
+    }
+
+
+    @DeleteMapping("/posts/{postId}")
+    public ResponseEntity<?> deletePost(@PathVariable Long postId) {
+        return postRepository.findById(postId).map(post -> {
+            postRepository.delete(post);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException("PostId " + postId + " not found"));
+    }
+
+}
