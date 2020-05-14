73a2bde906c04ff157218b80fcb9a020b9344246
Rosary7 <amirtrichy@gmail.com>
2020-05-14 12:25:53 +0530

    Update BootRestHibernateDsJavaConfigApplication.java

--- BootProjects/Boot_Rest_Hibernate_DS_JavaConfig_Aspect/src/main/java/com/sb/BootRestHibernateDsJavaConfigApplication.java
+++ BootProjects/Boot_Rest_Hibernate_DS_JavaConfig_Aspect/src/main/java/com/sb/BootRestHibernateDsJavaConfigApplication.java
@@ -1,13 +1,32 @@
 package com.sb;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BootRestHibernateDsJavaConfigApplication {
+import com.sb.aspect.EmpService;
 
+@SpringBootApplication
+public class BootRestHibernateDsJavaConfigApplication implements CommandLineRunner{
+
+	@Autowired
+	private EmpService empService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BootRestHibernateDsJavaConfigApplication.class, args);
 	}
 
+	
+	@Override
+	public void run(String... args) throws Exception {
+		 
+		try {
+			 empService.throwException();
+		} catch (Exception e) {
+			// Need not handle. We have handled with Aspect  
+		}
+ 
+	}
+	
 }
