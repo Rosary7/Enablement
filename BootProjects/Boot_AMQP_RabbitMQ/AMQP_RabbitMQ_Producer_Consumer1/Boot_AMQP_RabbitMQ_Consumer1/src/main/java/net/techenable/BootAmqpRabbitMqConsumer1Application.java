32348247c30f3605400b93b87b29888f7a2dbb3a
Rosary7 <amirtrichy@gmail.com>
2020-05-14 12:40:23 +0530

    Create BootAmqpRabbitMqConsumer1Application.java

---
+++ BootProjects/Boot_AMQP_RabbitMQ/AMQP_RabbitMQ_Producer_Consumer1/Boot_AMQP_RabbitMQ_Consumer1/src/main/java/net/techenable/BootAmqpRabbitMqConsumer1Application.java
@@ -0,0 +1,13 @@
+package net.techenable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAmqpRabbitMqConsumer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootAmqpRabbitMqConsumer1Application.class, args);
+	}
+
+}
