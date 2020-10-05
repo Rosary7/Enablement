b74576f5ce927b153a12f03ac5514e26cb269719
Rosary7 <amirtrichy@gmail.com>
2020-10-05 23:57:00 +0530

    Project
    Project

---
+++ BootProjects/boot-rest-crud/src/main/java/learn/springrest/crud/profile/Project.java
@@ -0,0 +1,15 @@
+package learn.springrest.crud.profile;
+
+public class Project {
+	private String projectCodeVersion;
+
+	public String getProjectCodeVersion() {
+		return projectCodeVersion;
+	}
+
+	public void setProjectCodeVersion(String projectCodeVersion) {
+		this.projectCodeVersion = projectCodeVersion;
+	}
+	
+
+}
